4cc80c29f317aabdf55317a889cdf6c45e88f500
Desman012 <desmandwisaputra@gmail.com>
2022-06-26 23:46:28 +0700

    update

--- Desman-1.py
+++ Desman-1.py
@@ -1,5 +1,5 @@
-status = (string(input("Apa status anda (on/off)?")))
+status = (str(input("Apa status anda (on/off)?")))
 if status == "off" or status == "OFF" or status == "Off":
     print("Izin Off Bro...")
-    else:
-        print("Ay ay captain")
+else : 
+    print("Ay ay captain")
